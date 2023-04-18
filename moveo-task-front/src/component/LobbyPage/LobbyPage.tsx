106d2d49fdc546b28c607c85b6247b1bad5d043a
rotem huga <rotem.3007@gmail.com>
2023-04-18 22:25:54 +0300

    adding socket packages - 18.04

--- moveo-task-front/src/component/LobbyPage/LobbyPage.tsx
+++ moveo-task-front/src/component/LobbyPage/LobbyPage.tsx
@@ -1,63 +1,3 @@
-// import "../LobbyPage/LobbyPage.css";
-// import { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { AllCodesValue, ICode } from "../../store/slices/codesSlice";
-// // import { fetchCodes } from "../../store/slices/codesSlice";
-// import { IRootState } from "../../store/store";
-
-// const LobbyPage: React.FC = () => {
-//   const dispatch = useDispatch();
-//   const allCodes: any = useSelector(
-//     (state: IRootState) => state.codes.value
-//   );
-//   const [filteredCodes, setFilteredCodes] = useState<ICode[]>([]);
-//   const [data, setData] = useState<any>(null);
-
-// //   useEffect(() => {
-// //     dispatch(fetchCodes());
-// //   }, [dispatch]);
-
-//   const handleAsyncCode = async () => {
-//     try {
-//       const response = await fetch("http://localhost:8000/moveoTask/codes", {
-//         method: "GET",
-//       });
-//       const data = await response.json();
-//       setData(data);
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   };
-
-//   const handleRedux = () => {
-//     const filtered = allCodes.filter((restObj: ICode) => restObj.title === "try");
-//     setFilteredCodes(filtered);
-//   };
-
-//   const handleStyleComponent = () => {
-//     // implement some styling logic here
-//   };
-
-//   return (
-//     <div>
-//       <h1 className="lobby-title">Lobby Page</h1>
-//       <button onClick={handleAsyncCode}>Async code</button>
-//       <button onClick={handleRedux}>Redux</button>
-//       <button onClick={handleStyleComponent}>Style component</button>
-//       {filteredCodes.map((code: ICode) => (
-//         <div key={code.title}>
-//           <h2>{code.title}</h2>
-//           <p>{code.code}</p>
-//         </div>
-//       ))}
-//       <div>{data}</div>
-//     </div>
-//   );
-// };
-
-// export default LobbyPage;
-
-
 import "../LobbyPage/LobbyPage.css"
 import { AllCodesValue, ICode } from "../../store/slices/codesSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -65,21 +5,21 @@ import { useEffect, useState } from "react";
 import { IRootState } from "../../store/store";
 import { ICodeState } from "../../store/store";
 
-const Codes:any = async () => {
-    const [data, setData] = useState<any>(null);
-	try {
-		const response = await fetch("http://localhost:8000/moveoTask/codes", {
-			method: "GET",
-		});
-		const data = await response.json();
-        setData(data);
-		return data;
-	} catch (err) {
-		console.log();
-	}
+// const Codes:any = async () => {
+//     const [data, setData] = useState<any>(null);
+// 	try {
+// 		const response = await fetch("http://localhost:8000/moveoTask/codes", {
+// 			method: "GET",
+// 		});
+// 		const codesData = await response.json();
+//         setData(codesData);
+// 		return codesData;
+// 	} catch (err) {
+// 		console.log();
+// 	}
 
 
-// const LobbyPage: React.FC = () => {
+const LobbyPage: React.FC = () => {
     // const dispatch = useDispatch();
     // const allCodes:any = useSelector(
     //     (state:IRootState) => state.codes.value
@@ -100,12 +40,10 @@ const Codes:any = async () => {
             </button>
             <button>
                 style component
-                {data.title}
             </button>
-            <div>{data.title}</div>
+            {/* <div>{allCodes}</div> */}
         </div>
     )
 }
-// };
-export default Codes
+export default LobbyPage
 
--- moveo-task-front/src/store/slices/codesSlice.tsx
+++ moveo-task-front/src/store/slices/codesSlice.tsx
@@ -7,33 +7,73 @@ export interface ICode {
 	code: string;
 }
 
-const dataCodes = fetch("http://localhost:8000/moveoTask/codes")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-      return data;
-    })
-    .catch((err:any) => {
-        console.log(err.message);
-    });
-
-
-const codes: any = await dataCodes;
-export const codeSlice = createSlice({
+const dataCodes = async () => {
+	try {
+		const response = await fetch(
+			"http://localhost:8000/moveoTask/codes",
+			{
+				method: "GET",
+			}
+		);
+		const codesData = await response.json();
+		return codesData;
+	} catch (err) {
+	}
+};
+const codes: ICode[] = await dataCodes();
+export const CodeSlice = createSlice({
 	name: "codes",
 	initialState: {
-		value: codes
+		value: codes,
+		// filteredValue: {},
 	},
 	reducers: {
 		AllCodesValue: (state) => {
-            state.value = state.value;
+			state.value = state.value;
 		},
 	},
 });
 
-export const { AllCodesValue } = codeSlice.actions;
+export const { AllCodesValue } = CodeSlice.actions;
 
-export default codeSlice.reducer;
+export default CodeSlice.reducer;
+
+
+// import { createSlice } from "@reduxjs/toolkit";
+// import { ObjectId } from "mongoose";
+
+// export interface ICode {
+// 	_id: ObjectId;
+// 	title: string;
+// 	code: string;
+// }
+
+// const dataCodes = fetch("http://localhost:8000/moveoTask/codes")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       return data;
+//     })
+//     .catch((err:any) => {
+//         console.log(err.message);
+//     });
+
+
+// const codes: any = await dataCodes;
+// export const codeSlice = createSlice({
+// 	name: "codes",
+// 	initialState: {
+// 		value: codes
+// 	},
+// 	reducers: {
+// 		AllCodesValue: (state) => {
+//             state.value = state.value;
+// 		},
+// 	},
+// });
+
+// export const { AllCodesValue } = codeSlice.actions;
+
+// export default codeSlice.reducer;
 
     // try {
 	// 	const response = await fetch(
