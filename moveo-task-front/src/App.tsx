0e575efd61707afe469dad5371fe2d4b66db5fcb
rotem huga <rotem.3007@gmail.com>
2023-04-18 23:39:26 +0300

    adding simple socket to the project - working - 18.04

--- moveo-task-front/src/App.tsx
+++ moveo-task-front/src/App.tsx
@@ -2,18 +2,43 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LobbyPage from "../src/component/LobbyPage/LobbyPage"
+import { io } from 'socket.io-client';
+import { useEffect, useState } from 'react';
 
+const socket = io("http://localhost:8000")
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  const sendMessage = () => {
+    socket.emit("send_message", { message });
+  };
+
+  useEffect (() => {
+    socket.on("recieve_message", (data) => {
+      setMessageReceived(data.message)
+    })
+  }, [socket])
+
   return (
-		<BrowserRouter>
-      <Routes>
-        <Route
-            path="/"
-            element={<LobbyPage />}
-				/>
-      </Routes>
-		</BrowserRouter>
+    <div>
+      <input type="text" 
+      placeholder='Message' 
+      onChange={(event) => {
+        setMessage(event.target.value)
+      }} />
+      <button onClick={sendMessage}>send message</button>
+      <h1>Message:</h1>
+      {messageReceived}
+    </div>
+		// <BrowserRouter>
+    //   <Routes>
+    //     <Route
+    //         path="/"
+    //         element={<LobbyPage />}
+		// 		/>
+    //   </Routes>
+		// </BrowserRouter>
   );
 }
 
